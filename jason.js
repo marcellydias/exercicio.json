023bca15516f30aee260ae1c0670900120c8c64f
Marcelly Dias <111815492+marcellydias@users.noreply.github.com>
2024-04-09 14:45:30 -0300

    Create jason.js

---
+++ jason.js
@@ -0,0 +1,89 @@
+// Aluno
+let aluno = {
+  "nome": "Marcelly",
+  "matricula": "622220718",
+  "curso": "Análise e Desenvolvimento de Sistemas",
+  "idade": 20,
+  "turno": "noite"
+};
+
+// Professor
+let professor = {
+  "nome": "Daniel",
+  "disciplinas": ["Usabilidade, Desenvolvimento Web, Mobile e Jogos "]
+};
+
+// Universidade
+let universidade = {
+  "nome": "Universidade UNA",
+  "campus": "Betim"
+};
+
+// Pessoa
+let pessoa = {
+  "nome": "Bella Hadid",
+  "idade": 27,
+  "sexo": "Feminino",
+  "profissao": "Modelo"
+};
+
+// Ligação Telefônica
+let ligacao = {
+  "tipo": "local",
+  "duracao": "5 minutos",
+  "numero_origem": "(31) 9999-8888",
+  "numero_destino": "(31) 7777-6666"
+};
+
+// Animal
+let animal = {
+  "especie": "Gato",
+  "raca": "Siamês",
+  "idade": 3,
+  "cor": "Laranja"
+};
+
+// Carro
+let carro = {
+  "marca": "Mercedes",
+  "modelo": "G63",
+  "ano": 2024,
+  "cor": "Preto"
+};
+
+// Endereco
+let endereco = {
+  "logradouro": "Rua ABC",
+  "numero": "123",
+  "bairro": "Centro",
+  "cidade": "Betim",
+  "estado": "MG",
+  "cep": "01000-000"
+};
+
+// Jogador de Futebol
+let jogadorFutebol = {
+  "nome": "Matheus Pereira",
+  "posicao": "Atacante",
+  "clube": "Cruzeiro",
+  "idade": 27
+};
+
+// Cartão de Crédito
+let cartaoCredito = {
+  "numero": "1234 5678 9012 3456",
+  "titular": "Marcelly Linda",
+  "validade": "12/24",
+  "codigo_seguranca": "123"
+};
+
+console.log(aluno);
+console.log(professor);
+console.log(universidade);
+console.log(pessoa);
+console.log(ligacao);
+console.log(animal);
+console.log(carro);
+console.log(endereco);
+console.log(jogadorFutebol);
+console.log(cartaoCredito);
